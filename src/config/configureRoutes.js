7d85773a10974e7bab41179ac0417f27d871ce31
soundz77 <simon_ap@gmx.net>
2024-07-19 13:41:56 +0200

    Added /auth

--- src/config/configureRoutes.js
+++ src/config/configureRoutes.js
@@ -1,7 +1,9 @@
 import userRouter from "../routes/users.js";
+import authRouter from "../routes/auth.js";
 
 const configureRoutes = (app) => {
   app.use("/api/v1/users", userRouter);
+  app.use("/api/v1/auth", authRouter);
   app.use("*", (req, res) => {
     res.status(404).json({
       message: `Can't find ${req.originalUrl} on this server!`,
