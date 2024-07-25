07823c0190401bca109360202c59ba5c54348024
soundz77 <simon_ap@gmx.net>
2024-07-25 16:49:37 +0200

    Minor middleware

--- base-template/src/config/configureMiddleware.js
+++ base-template/src/config/configureMiddleware.js
@@ -1,6 +1,5 @@
 import express from "express";
 import RateLimit from "express-rate-limit";
-import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
@@ -17,8 +16,7 @@ const __dirname = path.dirname(__filename);
 const configureMiddleware = (app) => {
   app.use(httpLogger);
   app.use(helmet(helmetConfig));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(RateLimit(rateLimitConfig));
   app.use(compression());
