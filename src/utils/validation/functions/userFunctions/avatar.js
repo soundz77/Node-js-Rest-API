14e7f5612da5ff6a4d5aad793f170281d10227a8
soundz77 <simon_ap@gmx.net>
2024-07-29 12:54:41 +0200

    Improved basic validation

--- src/utils/validation/functions/userFunctions/avatar.js
+++ src/utils/validation/functions/userFunctions/avatar.js
@@ -1,7 +1,7 @@
 // MISSING: detailed validation once requirements are clear
 
 const validateAvatar = (value) => {
-  return value && typeof (value === "string");
+  return value && typeof (value === "string") && ["1.png", "2.png", "3.png"].includes(value);
 };
 
 export default validateAvatar;
