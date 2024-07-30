4461fa996bd7ba8c01e373694882e85516b344e5
soundz77 <simon_ap@gmx.net>
2024-07-30 13:40:19 +0200

    Refactored

--- src/utils/requests/handleRequest.js
+++ src/utils/requests/handleRequest.js
@@ -5,39 +5,32 @@ import { validationResult, matchedData } from "express-validator";
 import checkVarsSet from "../../../base-template/src/utils/validation/checkVarsSet.js";
 
 const handleRequest = handleAsync(async (req, res) => {
-  const messages = loggingMessages.crud.errors.invalidRequest;
+  const messages = loggingMessages.crud.errors;
 
-  checkVarsSet({ req, res, messages });
+  // Check necessary variables have been set
+  checkVarsSet({ req, res, messages: messages.invalidRequest });
 
   // Ensure req.body is not empty and that req.params and req.query are empty
-  const requestError = requestUtils.checkRequestErrors(req, res, messages);
-
+  const requestError = requestUtils.checkRequestErrors(req, res, messages.invalidRequest);
   if (requestError) {
     return res.status(400).json({ message: requestError });
   }
 
   // Check for duplicate keys in the request body
-  const keys = Object.keys(req.body);
-  const uniqueKeys = new Set(keys);
-
-  if (uniqueKeys.size !== keys.length) {
-    return res
-      .status(400)
-      .json({ message: "Duplicate keys found in the request body" });
+  if (requestUtils.checkNoDuplicates(req.body)) {
+    return res.status(400).json({ message: messages.duplicateFields});
   }
 
-  // Get validation messages (using the schema applied to the route)
+  // Get validation results from the schema applied in the route definition
   const result = validationResult(req);
 
-  // If validation failed, return the first error<
+  // If validation failed, return the first error
   if (!result.errors) {
     return requestUtils.hasValidationErrors(req, res, result.errors);
   }
 
-  // If validation passes, get the validated data
-  const validatedData = matchedData(req);
-
-  return validatedData;
+  // If validation passes, return validated data
+  return matchedData(req);
 });
 
 export default handleRequest;
