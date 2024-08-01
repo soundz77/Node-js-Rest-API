3e2ffd1abe1894864a07a6ff9284e604a35b0bcf
soundz77 <simon_ap@gmx.net>
2024-08-01 16:47:14 +0200

    Added token router

--- src/config/configureRoutes.js
+++ src/config/configureRoutes.js
@@ -1,9 +1,11 @@
 import userRouter from "../routes/users.js";
 import authRouter from "../routes/auth.js";
+import tokenRouter from "../routes/tokens.js";
 
 const configureRoutes = (app) => {
   app.use("/api/v1/users", userRouter);
   app.use("/api/v1/auth", authRouter);
+  app.use("/api/v1/tokens", tokenRouter);
   app.use("*", (req, res) => {
     res.status(404).json({
       message: `Can't find ${req.originalUrl} on this server!`,
