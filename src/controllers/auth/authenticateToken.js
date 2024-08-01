04810536c76570a6b7eb29e5a315b4b5ad452534
soundz77 <simon_ap@gmx.net>
2024-08-01 16:48:14 +0200

    Minor

--- src/controllers/auth/authenticateToken.js
+++ src/controllers/auth/authenticateToken.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -13,29 +13,9 @@ const authenticateToken = (req, res, next) => {
     if (err) {
       return res.sendStatus(403); // Forbidden
     }
-      req.user = user;
+    req.user = user;
     next();
   });
 };
 
 export default authenticateToken;
-
-
-/*
-import jwt from 'jsonwebtoken';
-
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (token == null) return res.sendStatus(401); // No token
-
-  jwt.verify(token, 'your-secret-key', (err, user) => {
-    if (err) return res.sendStatus(403); // Invalid token
-    req.user = user; // Attach user info to request
-    next();
-  });
-};
-
-app.use(authenticateToken);
-*/
